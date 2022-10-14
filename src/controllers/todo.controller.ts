0fffed9f75cc4ee177cb41623cc7ddd292dd55c1
Jeon-man <gusals236@gmail.com>
2022-10-14 14:59:18 +0900

    DOCS: swagger decorators add

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -9,7 +9,7 @@ import {
   Post,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { TodoService } from '@service';
 
@@ -23,31 +23,55 @@ import { CreateTodoDto, UpdateTodoDto } from '@dto/todo';
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
+  @ApiOperation({
+    summary: '모든 투두',
+    description: '모든 todo를 불러옵니다.',
+  })
   @Get()
   async findAll() {
     return this.todoService.findAll();
   }
 
+  @ApiOperation({
+    summary: '유저 todo',
+    description: '유저의 모든 todo를 불러옵니다.',
+  })
   @Get('/user/:id')
   async findUserTodo(@Param('id', ParseIntPipe) id: number) {
     return this.todoService.findUserTodo(id);
   }
 
+  @ApiOperation({
+    summary: '투두',
+    description: 'todo 한 개를 불러옵니다.',
+  })
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
     return this.todoService.findById(id);
   }
 
+  @ApiOperation({
+    summary: 'Todo 추가',
+    description: 'todo 한 개를 만듭니다.',
+  })
   @Post(':id/add')
   async addTodo(@Body() creationTodo: CreateTodoDto, @Param('id', ParseIntPipe) id: number) {
     return this.todoService.create(creationTodo, id);
   }
 
+  @ApiOperation({
+    summary: 'todo 수정',
+    description: 'todo의 내용을 수정합니다.',
+  })
   @Patch(':id')
   async update(@Param('id', ParseIntPipe) id: number, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todoService.update(id, updateTodoDto);
   }
 
+  @ApiOperation({
+    summary: 'todo 삭제',
+    description: 'todo를 삭제합니다.',
+  })
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
     return this.todoService.delete(id);
